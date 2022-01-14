505f67c32f59e2d83120a9e235e6ba3772663ce0
jfernandesdev <jfernandes.dev@gmail.com>
2022-01-13 22:36:50 -0300

    import of global icons

---
+++ src/styles/Icons.js
@@ -0,0 +1,14 @@
+export {
+  ShoppingBag, 
+  Plus, 
+  ArrowLeft, 
+  Star
+} from 'styled-icons/heroicons-solid';
+
+export { Search } from 'styled-icons/heroicons-outline';
+
+export {
+  Facebook, 
+  Twitter, 
+  Instagram
+} from 'styled-icons/boxicons-logos';
